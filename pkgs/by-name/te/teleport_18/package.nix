2a26aa5bd52da0f2dc88651bf5f65e7bc6463943
Justinas Stankevicius <justinas@justinas.org>
2025-09-06 15:21:08 +0300

    teleport_18: 18.1.1 -> 18.2.0

--- pkgs/by-name/te/teleport_18/package.nix
+++ pkgs/by-name/te/teleport_18/package.nix
@@ -4,11 +4,11 @@
   wasm-bindgen-cli_0_2_99,
 }:
 
-buildTeleport rec {
-  version = "18.1.1";
-  hash = "sha256-xhf6WwgR3VwjtvFo0/b9A0RcyY7dklPfPUakludUmm8=";
-  vendorHash = "sha256-63pqTI92045/V8Gf+TDKUWLV9eO4hVKOHtgWbYnAf6I=";
-  pnpmHash = "sha256-ZuMMacsyr2rGLVDlaEwA7IbZZfGBuTRBOv4Q6XIjDek=";
+buildTeleport {
+  version = "18.2.0";
+  hash = "sha256-JWgGRv9pK76u7IxwqnBcuAI93XIKfIVvme7l+a/3J7c=";
+  vendorHash = "sha256-oPi/rIuwze2ZlyHfZ2MdDHHvdIaF2IZ2aklEVNRgoLo=";
+  pnpmHash = "sha256-wW4RT1uqOTpy8wKIsAOfIlxoOamTzPqEbFQRAub+sn4=";
   cargoHash = "sha256-ia4We4IfIkqz82aFMVvXdzjDXw0w+OJSPVdutfau6PA=";
 
   wasm-bindgen-cli = wasm-bindgen-cli_0_2_99;
